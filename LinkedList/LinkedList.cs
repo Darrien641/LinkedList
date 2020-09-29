a9c43f5de726a61a8e452c79abed42fa83879f83
DARRIEN GONZALEZ <DARRIEN.GONZALEZ@student.centralia.edu>
2020-09-29 15:26:04 -0700

    Add project files.

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    class LinkedList
+    {
+        private Node front;
+
+        public Node Add(string data)
+        {
+            if (front == null)
+            {
+                front = new Node(data);
+                return front;
+            }
+            Node current = front;
+            while (current != null)
+            {
+                
+            }
+            return front;
+        }
+
+        public Node addFirst(string data)
+        {
+            if (front == null)
+            {
+                front = new Node(data);
+                return front;
+            }
+            else
+            {
+                Node current = front;
+                front = new Node(data);
+                return front;
+            }
+        }
+    }
+}
